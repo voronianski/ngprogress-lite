0c1bb6d5f15ff97183a45b9d15b4469cd6cba547
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2013-10-07 21:49:11 +0300

    add jshint && minification

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> (<%= pkg.homepage %>) */\n',
+				mangle: false
+			},
+			avgrund: {
+				files: {
+					'ngprogress-lite.min.js': ['ngprogress-lite.js']
+				}
+			}
+		},
+		jshint: {
+			options: {
+				ignores: ['ngprogress-lite.min.js']
+			},
+			files: ['*.js']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['jshint', 'uglify']);
+};
